932f66e2e033310d44ef9b26379e995bb2ada1bf
zirem11 <89360252+zirem11@users.noreply.github.com>
2023-05-04 21:17:51 +0300

    Add files via upload
    Projemin bu kısmında personel giriş kısmını tamamladım. Ancak bunu veritabanıyla yapmayı başaramadığım için şimdilik if-else ile yaptım. Veritabanı ile yapmaya çalışacağım. Yapabilirsem değiştireceğim.

---
+++ Doğal Taş Bilgilendirme Sayfası Temel Arayüz - Kopya/Doğal Taş Bilgilendirme Sayfası Temel Arayüz/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doğal_Taş_Bilgilendirme_Sayfası_Temel_Arayüz
+{
+    public partial class frmanasayfa : Form
+    {
+        public frmanasayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnpersonelgiris_Click(object sender, EventArgs e)
+        {
+            //fpg=form personel giriş
+            frmpersonelgiris fpg = new frmpersonelgiris();
+            fpg.Show();
+            this.Hide();
+        }
+    }
+}
