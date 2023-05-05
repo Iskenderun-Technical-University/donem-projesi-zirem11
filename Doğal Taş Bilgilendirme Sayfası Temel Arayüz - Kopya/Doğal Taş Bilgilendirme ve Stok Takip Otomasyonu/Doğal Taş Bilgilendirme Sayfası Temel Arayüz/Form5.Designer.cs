c5537204b3de6336cfac7bd23aa65fb4859a5430
zirem11 <89360252+zirem11@users.noreply.github.com>
2023-05-05 18:53:13 +0300

    Add files via upload
    Anasayfada kullandığım butonları -1 buton hariç- aktif hale getirdim.

---
+++ Doğal Taş Bilgilendirme Sayfası Temel Arayüz - Kopya/Doğal Taş Bilgilendirme ve Stok Takip Otomasyonu/Doğal Taş Bilgilendirme Sayfası Temel Arayüz/Form5.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Doğal_Taş_Bilgilendirme_Sayfası_Temel_Arayüz
+{
+    partial class frmbilgilendirme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(619, 99);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bu otomasyonda satış işlemi yapılmamaktadır. Kodlama bilgi \r\nseviyemin artışına g" +
+    "öre güncellemeler yapılacaktır.\r\nTAKİPTE KALINNN :)))\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(22, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Önceki sayfa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmbilgilendirme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(674, 340);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmbilgilendirme";
+            this.Text = "Bilgilendirme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
